9c0c226f1e455be48d2500b0bfc7e4fb1d735c13
Bahadır KAYHAN <kayhan1901@gmail.com>
2021-03-08 14:25:45 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+      
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi: "+musteri.Ad+" "+musteri.Soyad+"\n");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi: " + musteri.Ad + " " + musteri.Soyad + "\n");
+        }
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi: " + musteri.Ad + " " + musteri.Soyad + "\n");
+        }
+
+        public void Güncelle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Güncellendi: " + musteri.Ad + " " + musteri.Soyad + "\n");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager metodlar = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id =1;
+            musteri1.Ad ="Dilvin";
+            musteri1.Soyad ="ENSARİ";
+            musteri1.Yas =17;
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Bahadır";
+            musteri2.Soyad = "KAYHAN";
+            musteri2.Yas = 20;
+            
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Yıldıray";
+            musteri3.Soyad = "KAYHAN";
+            musteri3.Yas = 20;
+
+            
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach (Musteri musteriListele in musteriler)
+            {
+                Console.WriteLine("Id: "+musteriListele.Id);
+                Console.WriteLine("Ad: "+musteriListele.Ad);
+                Console.WriteLine("Soyad: "+musteriListele.Soyad);
+                Console.WriteLine("Yaş "+musteriListele.Yas);
+
+                metodlar.Listele(musteriListele);
+
+            }
+
+            foreach (Musteri MusteriEkle in musteriler)
+            {
+                metodlar.Ekle(MusteriEkle);
+            }
+
+            foreach (Musteri MusteriSil in musteriler)
+            {
+                metodlar.Sil(MusteriSil);
+            }
+
+            foreach (Musteri MusteriGüncelle in musteriler)
+            {
+                metodlar.Güncelle(MusteriGüncelle);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
